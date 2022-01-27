845ea53393fb59b6089f9d2e2548a8f73c5d352d
Sidia <sidia.reis1@gmail.com>
2022-01-26 18:05:10 -0800

    create routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const RekognitionServiceController = require("../controllers/RekognitionServiceController")
-const S3ServiceController = require("../controllers/S3ServiceController")
+const RekognitionController = require("../controllers/RekognitionController")
+//const S3ServiceController = require("../controllers/S3ServiceController")
 const uploadImage = require("../config/multer")
 
 
-router.get('/checkin', uploadImage, RekognitionServiceController.compareFaces);
-router.post('/addFaceToIndex', uploadImage, RekognitionServiceController.addProfilePicture );
-router.get('/analyze',uploadImage, RekognitionServiceController.analyzeFace);
+router.get('/create', uploadImage, RekognitionController.createNewCollection);
+router.post('/analyze', uploadImage, RekognitionController.analyzeText );
 //s3
-router.post('/upload', S3ServiceController.uploadSinglePicture)
+//router.post('/upload', S3ServiceController.uploadSinglePicture)
 
 module.exports = router;
