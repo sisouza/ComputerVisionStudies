2fbb899446292b94a59d6cb170e6ea4ef5a69d4b
Sidia <sidia.reis1@gmail.com>
2022-01-26 18:05:31 -0800

    create service

--- services/RekognitionService.js
+++ services/RekognitionService.js
@@ -5,101 +5,34 @@ const { AWS_REGION } = process.env;
 
 const rekognition = new aws.Rekognition();
 
-class RekoService {
+class RekognitionService {
 
-    createCollection(colectionName){
-       const params = {
-            CollectionId: colectionName
-          };
-        
-          rekognition.createCollection(params, function(err, data) {
-            if (err) {
-              console.log(err, err.stack);
-            } else {
-              console.log(data);
-            }
-          });
-    
-    }
+    detectText(file) {
+        const params = {
+           CollectionId: "test",
+           Image: {
+           Bytes: file
+           }
+       };
 
-     addFaceToIndex(file) {
-         const params = {
-            CollectionId: "test",
-            Image: {
-            Bytes: file
-            },
-            DetectionAttributes: ["ALL"],
-            ExternalImageId: file.originalname,
-            MaxFaces: 1,
-            QualityFilter: "AUTO"
-        };
-        
-        rekognition.indexFaces(params, function(err, data) {
+       rekognition.detectText(params, function (err, data) {
         if (err) {
-        console.log(err, err.stack);
+            console.log(err, err.stack);
         } else {
-        console.log(data);
+
+            for (var i = 0; i < data.TextDetections.length; i++) {
+               var result = {
+              "detectedtext":data.TextDetections[i].DetectedText,
+               "texttype": data.TextDetections[i].Type,
+                "confidence": data.TextDetections[i].Confidence
+                }
+                console.log(result)
             }
-        });
-    }
-
-
-    detectFace(file) {
-
-        var params = {
-                Image: {
-                    Bytes: file
-                },
-                Attributes: ["ALL"]
-            };
             
-        
-        rekognition.detectFaces(params, function (err, data) {
-            if (err) {
-                console.log(err, err.stack);
-            } else {
-              
-                 for (var i = 0; i < data.FaceDetails.length; i++) {
-                    var result = {
-                        "age range low": data.FaceDetails[i].AgeRange.Low,
-                        "age range high": data.FaceDetails[i].AgeRange.High,
-                        "gender": data.FaceDetails[i].Gender.Value,
-                        "emotion type": data.FaceDetails[i].Emotions[0].Type
-                    }
-                    console.log(result)
-                }
-
-            }
-         })
         }
-
-   
-    compareFaces(file) {
-
-         var params = {
-            CollectionId: "users",
-            Image: {
-            Bytes: file
-            },
-            FaceMatchThreshold: 90,
-            MaxFaces: 10
-         };
-        
-        rekognition.searchFacesByImage(params, function(err, data) {
-            if (err) {
-                console.log(err, err.stack);
-            } else {
-           
-                for (var i = 0; i < data.FaceMatches.length; i++) {
-                    var result = {
-                        "similiriarity": data.FaceMatches[i].Similarity,
-                        "userMatch": data.FaceMatches[i].Face.ExternalImageId
-                    }
-                    console.log(result)
-                }
-            }
-        });
+     });
     }
+
 
 }
 
